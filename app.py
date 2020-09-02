b96936da539903034a0e2b86d7f0e9186ffbe3b0
Sheen Dong <53708497+dxljack@users.noreply.github.com>
2020-09-01 23:21:55 -0400

    change server_document

--- app.py
+++ app.py
@@ -91,8 +91,8 @@ def bkapp(doc):
 
 @app.route('/', methods=['GET'])
 def bkapp_page():
-    script = server_document('https://desolate-island-86478.herokuapp.com/bkapp')
-    # script = server_document()
+    # script = server_document('http://localhost:5006/bkapp')
+    script = server_document()
     return render_template("embed.html", script=script, template="Flask")
 
 
